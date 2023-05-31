36d9ff35a6baf7427a3df2a23a668fbe66748ef4
Rizwan Nasir <97438351+RizwanNaasir@users.noreply.github.com>
2023-05-31 12:53:21 +0500

    updated: meta tags

--- docs/meta.js
+++ docs/meta.js
@@ -1,15 +1,3 @@
-const ogImageMeta = document.getElementsByClassName("og-image")
-
-for (let i = 0; i < ogImageMeta.length; i++) {
-    ogImageMeta[i].setAttribute("content", window.location + ogImageMeta[i].getAttribute("content"))
-}
-
-const ogDescriptionMeta = document.getElementsByClassName("og-description")
-
-for (let i = 0; i < ogDescriptionMeta.length; i++) {
-    ogDescriptionMeta[i].setAttribute("content", "Rizwan Nasir — Full Stack Web Developer | Laravel | Symfony | Vue.js | React | Tailwind CSS")
-}
-
 // Calculate the number of years of experience
 const currentYear = new Date().getFullYear();
 const startYear = 2020;
