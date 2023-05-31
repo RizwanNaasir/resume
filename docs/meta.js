144f6f2447c23ffb5e629596e83c4c5e4446aac6
Rizwan Nasir <97438351+RizwanNaasir@users.noreply.github.com>
2023-05-31 12:43:26 +0500

    added: social icons

--- docs/meta.js
+++ docs/meta.js
@@ -9,3 +9,12 @@ const ogDescriptionMeta = document.getElementsByClassName("og-description")
 for (let i = 0; i < ogDescriptionMeta.length; i++) {
     ogDescriptionMeta[i].setAttribute("content", "Rizwan Nasir — Full Stack Web Developer | Laravel | Symfony | Vue.js | React | Tailwind CSS")
 }
+
+// Calculate the number of years of experience
+const currentYear = new Date().getFullYear();
+const startYear = 2020;
+const yearsOfExperience = currentYear - startYear;
+
+// Update the content of the element with the years of experience
+const yearsOfExperienceElement = document.getElementById("yearsOfExperience");
+yearsOfExperienceElement.textContent = yearsOfExperience.toString();
