a7d0eb5fcdfd743bdeae618b5ab1e162455fa040
Rizwan Nasir <97438351+RizwanNaasir@users.noreply.github.com>
2023-05-31 10:55:27 +0500

    updated: meta tags

--- docs/meta.js
+++ docs/meta.js
@@ -3,3 +3,9 @@ const ogImageMeta = document.getElementsByClassName("og-image")
 for (let i = 0; i < ogImageMeta.length; i++) {
     ogImageMeta[i].setAttribute("content", window.location + ogImageMeta[i].getAttribute("content"))
 }
+
+const ogDescriptionMeta = document.getElementsByClassName("og-description")
+
+for (let i = 0; i < ogDescriptionMeta.length; i++) {
+    ogDescriptionMeta[i].setAttribute("content", "Rizwan Nasir — Full Stack Web Developer | Laravel | Symfony | Vue.js | React | Tailwind CSS")
+}
